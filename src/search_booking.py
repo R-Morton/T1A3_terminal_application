9e6f3914ca754ac702c62c4b75485d058d351038
R-Morton <remann12@gmail.com>
2022-12-10 12:35:59 +1100

    search works and able to view and select from matches

--- src/search_booking.py
+++ src/search_booking.py
@@ -1,10 +1,26 @@
 def search_booking():
     bookings_file = open("src/bookings_list.txt", "r")
     name_search = input("Please type the first or last name of the customer: ")
+    selected_booking = ""
     matching_names = []
+    results = 0
     for line in bookings_file.readlines():
         if name_search == line.split()[2] or name_search == line.split()[3]:
+            results += 1
             matching_names.append(line)
-    
+            print(f"{line}")
+        else:
+            continue
+    user_input = int(input("Please select from the matches: "))
+    count = 1
+    for booking in matching_names:
+        if user_input == count:
+            print(booking)
+            break
+        else:
+            count += 1
+            continue
+
+
 
 search_booking()
